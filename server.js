b893d1d8401ca80fea6b3e9f1d94b20974f6dc0f
kmfrick <kmfrick98@gmail.com>
2018-11-28 12:46:02 +0100

    implemented primitive battle queue

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+/* server.js */
+
 const port = process.env.PORT || 5000;
 const io = require('socket.io')();
 
@@ -15,11 +17,45 @@ app.get('/express_backend', (req, res) => {
 });
 */
 const connections = new Map();
+const battles = [];
+
+function getFirstWaitingBattle(battles) {
+    let i = 0;
+    let result = null;
+    battles.forEach(battle => {
+        let room = io.sockets.adapter.rooms[battle];
+        let clients = (typeof room !== 'undefined' ? room.sockets : undefined);
+        let numPlayers = (typeof clients !== 'undefined') ? Object.keys(clients).length : 0;
+        console.log(`${battle} has ${numPlayers} players`);
+        if (!result && (numPlayers < 2 || !numPlayers)) {
+            console.log(`found battle${i} waiting`);
+            result = battle;
+        } else {
+            i++;
+        }
+    });
+    return result;
+}
 
 io.on('connection', function (c) {
 
     connections.set(c, c);
     console.log('connected ', c.id);
+
+    let firstWaitingBattle = getFirstWaitingBattle(battles);
+    let battleToJoin = firstWaitingBattle;
+    console.log(`firstWaitingBattle = ${firstWaitingBattle}`);
+    if (!battles.length || !firstWaitingBattle) {
+        battleToJoin = `battle${battles.length}`;
+        console.log(`creating battle${battles.length}`);
+        battles.push(battleToJoin);
+    }
+    io.to(c.id).emit('battle id', battleToJoin);
+    c.join(battleToJoin);
+    io.in(battleToJoin).clients((err, clients) => {
+        console.log(clients);
+    });
+
 
     c.once('disconnect', function () {
         connections.delete(c);
@@ -30,3 +66,4 @@ io.on('connection', function (c) {
 
 io.listen(port);
 console.log('socket.io listening on ', port);
+
