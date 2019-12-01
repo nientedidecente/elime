b8acee6f0db6e8a6cd50bfee5c0861f449788f50
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2019-12-01 19:14:49 +0000

    moved svg files to simple files

--- src/components/cards/elements/Icon.js
+++ src/components/cards/elements/Icon.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import './Icon.css';
-import {TYPES} from "../../../libs/models";
-import water from "../../../assets/img/elements/water.svg";
-import fire from "../../../assets/img/elements/fire.svg";
-import earth from "../../../assets/img/elements/earth.svg";
-import fallback from "../../../assets/img/unknown.svg";
+import { TYPES } from "libs/models";
 
 const elementsMapping = {
-    [TYPES.WATER]: water,
-    [TYPES.FIRE]: fire,
-    [TYPES.EARTH]: earth,
-    default: fallback
+    [TYPES.WATER]: 'assets/img/elements/water.svg',
+    [TYPES.FIRE]: 'assets/img/elements/fire.svg',
+    [TYPES.EARTH]: 'assets/img/elements/earth.svg',
+    default: 'assets/img/unknown.svg'
 };
 
-const Icon = ({element}) => <img className="elementIcon" src={elementsMapping[element] || elementsMapping.default} alt={element}/>;
-export {Icon};
+const Icon = ({ element }) => <img className="elementIcon" src={elementsMapping[element] || elementsMapping.default} alt={element} />;
+export { Icon };
